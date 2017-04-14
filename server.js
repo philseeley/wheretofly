968351ca544607dbdcf284932ce89ead12585ddd
Phil Seeley <phil.seeley@gmail.com>
2017-04-14 23:54:23 +0930

    We now log requests to run/access.log. This needs to be a rw mount now.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require("fs");
 var wtf = require('./wtf.js');
 wtf.retrieveForecast();
 
@@ -10,6 +11,7 @@ cron.schedule('45 1,4,7,10,13,16,19,22 * * *', function(){
 var httpServer = require('http');
 
 var http = httpServer.createServer(function(request, response) {
+
   if (request.url == "/") request.url = "/wtf.html";
   response.writeHead(301, {
   "Location": "https://"+request.headers.host+request.url
@@ -19,8 +21,18 @@ response.end();
 http.listen(8080);
 
 var httpsServer = require('http-server');
+var accesslog = require('access-log');
+var logFD = fs.openSync("run/access.log", "a");
+
+function logRequest(request, response)
+{
+  accesslog(request, response, null, function(s) {
+    fs.write(logFD, s+'\n');
+  });
+}
 
 var options = {
+  logFn: logRequest,
   https: {
     cert: 'run/cert.pem',
     key: 'run/key.pem'
