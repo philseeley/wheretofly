f2739c63bed8c1d07e0ffb69d9ead0457b4c38ce
Phil Seeley <phil.seeley@gmail.com>
2018-09-24 21:26:00 +1000

    Images also need the User-Agent.

--- wtf.js
+++ wtf.js
@@ -196,7 +196,11 @@ function imageCB(uri, filename)
 {
   if(!fs.existsSync(filename))
   {
-    request(uri).pipe(fs.createWriteStream(filename));
+    request(
+      {
+        url: uri,
+        headers: {'User-Agent': "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36"},
+      }).pipe(fs.createWriteStream(filename));
   }
 }
 
